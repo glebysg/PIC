8c74e8286357d2676d564367c0aa2dbca4a2d5d4
Glebys Gonzalez <glebysg@gmail.com>
2018-03-15 22:41:05 -0400

    ik unreachable working but called only once:w

--- fabrik.py
+++ fabrik.py
@@ -5,12 +5,12 @@ from vpython import *
 class ikLink:
     def __init__(self, length=1, orientation=[1,0,0]):
         self.length = length
-        self.orientation = np.array(orientation,dtype=float)
+        self.orientation = np.array(orientation, dtype=float)
 
 class ikChain:
     def __init__(self,base=[0,0,0],chain=[], tolerance=0.1, iterations=10):
         # a 3d point indicating the base of the kinematic chain
-        self.base = np.array(base,dtype=float)
+        self.base = np.array(base, dtype=float)
         # a list of iklinks
         self.chain = chain
         # error tolerance
@@ -38,17 +38,28 @@ class ikChain:
             axis = vec(*(normalize(self.chain[index].orientation)*self.chain[index].length))
             joint =  sphere(pos=pos,color=color.green, radius = 4)
             link = cylinder(pos=pos, axis=axis, radius=2)
-            self.chain.append(joint)
-            self.chain.append(link)
-        gripper = pyramid(pos=vec(*self.points[-1]), size=vec(2,4,4), axis=axis, color=color.green)
+            self.graphic_ik.append(joint)
+            self.graphic_ik.append(link)
+        self.gripper = pyramid(pos=vec(*self.points[-1]), size=vec(2,4,4), axis=axis, color=color.green)
         # Create the ik ball to manipulate the chain and bind the drag
         self.ik_sphere.pos=vec(*self.points[-1])
+        self.animation_pos=vec(*self.points[-1])
         def down():
             self.drag = True
         def move():
             if self.drag: # mouse button is down
                 self.ik_sphere.color = color.cyan
                 self.ik_sphere.pos = scene.mouse.pos
+            self.new_pos = self.ik_sphere.pos
+            if not self.animation_pos.equals(self.new_pos):
+                self.animation_pos = self.new_pos
+                #Get the target from the sphere
+                target = np.array([
+                  self.animation_pos.x,
+                  self.animation_pos.y,
+                  self.animation_pos.z,
+                  ], dtype=float)
+                self.solve(target)
         def up():
             self.ik_sphere.color = color.red
             self.drag = False
@@ -85,19 +96,20 @@ class ikChain:
             target_point = backward_point
     def solve(self, target):
         # Check if the point is reachable
-        self.target = np.array(target,ndtype=float)
+        self.target = np.array(target, dtype=float)
         distance_to_target = np.linalg.norm(self.target-self.base)
-        # if it's not reachable
+        # if the target is reachable
         if (sum([l.length for l in self.chain]) > distance_to_target):
             # Get goal orientation
             goal_orientation = self.target-self.base
             for i in range(len(self.chain)):
                 self.chain[i].orientation = goal_orientation
-        # if it's reachable
+            self.points = self.get_points()
+        # if the target is not reachable
         else:
             # initialize the points
             self.points = self.get_points()
-            # get distance between target and goal 
+            # get distance between target and goal
             error = distance(self.points[-1],self.target)
             count = 0
             while error > self.tolerance:
@@ -107,13 +119,22 @@ class ikChain:
                 if count > self.iterations:
                         break
                 count += 1
+        print("solved")
     def animate(self):
-      rate(100)
-      # self.t = self.t + self.dt
-      # self.ball.pos = self.ball.pos + (self.ball.p/self.ball.mass)*self.dt
-      # if not (self.side > self.ball.pos.x > -self.side):
-        # self.ball.p.x = -self.ball.p.x
-      # if not (self.side > self.ball.pos.y > -self.side):
-        # self.ball.p.y = -self.ball.p.y
-      # if not (self.side > self.ball.pos.z > -self.side):
-        # self.ball.p.z = -self.ball.p.z
+        rate(100)
+        # if the mouse is being dragged
+        if self.drag:
+            # Animate the solved ik chain
+            axis = None
+            for index in range(len(self.chain)):
+                # Normalize the orientation o f the ik link
+                pos = vec(*self.points[index])
+                axis = vec(*(normalize(self.chain[index].orientation)\
+                        *self.chain[index].length))
+                self.graphic_ik[index*2].pos = pos
+                self.graphic_ik[index*2+1].pos = pos
+                self.graphic_ik[index*2+1].axis = axis
+            self.gripper.pos = vec(*self.points[-1])
+            self.gripper.axis = axis
+            self.gripper.size = vec(2,4,4)
+
